a915c32cde527ae8e7739424903be97bc87eeb34
ZigTag <17077422+ZigTag@users.noreply.github.com>
2020-10-25 16:23:53 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import logo from './fbw.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+      return (
+          <div className="App">
+              <Toolbar/>
+          </div>
+      );
+  }
+}
+
+class Toolbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { itemList: ["Testing1", "Testing2"], activeIndex: 0 }
+    }
+
+    render() {
+        return (
+            <div className="Toolbar">
+                <div className="Toolbar-div">
+                    <img src={logo} className="FBW-Logo" alt="logo" />
+                    <ToolbarItem content="Testing1" />
+                </div>
+            </div>
+        );
+    }
+}
+
+class ToolbarItem extends React.Component {
+    render() {
+        return (
+            <div id="ToolbarItem">
+                <p>{this.props.content}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
