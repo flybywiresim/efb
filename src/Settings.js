a215d94444d3c31bfe3ed30397e266a4d013b22c
ZigTag <17077422+ZigTag@users.noreply.github.com>
2020-11-01 00:02:52 -0700

    Added the css for a button in the settings menu

--- src/Settings.js
+++ src/Settings.js
@@ -6,12 +6,15 @@ class Settings extends React.Component {
             <div className="Settings">
                 <div id="SettingsItem1" className="SettingsItem">
                     <p>Placeholder Data 1</p>
+                    <p className="Switch">Switch</p>
                 </div>
                 <div id="SettingsItem2" className="SettingsItem">
                     <p>Placeholder Data 2</p>
+                    <p className="Switch">Switch</p>
                 </div>
                 <div id="SettingsItem3" className="SettingsItem">
                     <p>Placeholder Data 3</p>
+                    <p className="Switch">Switch</p>
                 </div>
             </div>
         );
