12384e445b4c528cb9ef09505e8d4fe3864ce107
ZigTag <17077422+ZigTag@users.noreply.github.com>
2020-11-06 15:22:19 -0800

    Added in dark mode and toggles

--- src/Settings.js
+++ src/Settings.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
 class Settings extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {darkMode: document.getElementById("root").classList.contains("darkMode")};
+    }
+
+    handleDark() {
+        return () => {
+            const element = document.getElementById("root");
+            element.classList.toggle("darkMode");
+            this.setState({ darkMode: !this.state.darkMode });
+        };
+    }
+
     render() {
         return (
             <div className="Settings">
                 <div id="SettingsItem1" className="SettingsItem">
-                    <p>Placeholder Data 1</p>
-                    <p className="Switch">Switch</p>
+                    <p>Dark Mode</p>
+                    <p className="Switch" onClick={this.handleDark()}>{this.state.darkMode ? "On" : "Off"}</p>
                 </div>
                 <div id="SettingsItem2" className="SettingsItem">
                     <p>Placeholder Data 2</p>
