9da40ed4808787515ea6860e7d44ab78f08595b7
ZigTag <17077422+ZigTag@users.noreply.github.com>
2020-11-08 14:20:17 -0800

    Added more settings entries and made overflow not show

--- src/Settings.js
+++ src/Settings.js
@@ -29,6 +29,34 @@ class Settings extends React.Component {
                     <p>Placeholder Data 3</p>
                     <p className="Switch">Switch</p>
                 </div>
+                <div id="SettingsItem4" className="SettingsItem">
+                    <p>Placeholder Data 4</p>
+                    <p className="Switch">Switch</p>
+                </div>
+                <div id="SettingsItem5" className="SettingsItem">
+                    <p>Placeholder Data 5</p>
+                    <p className="Switch">Switch</p>
+                </div>
+                <div id="SettingsItem6" className="SettingsItem">
+                    <p>Placeholder Data 6</p>
+                    <p className="Switch">Switch</p>
+                </div>
+                <div id="SettingsItem7" className="SettingsItem">
+                    <p>Placeholder Data 7</p>
+                    <p className="Switch">Switch</p>
+                </div>
+                <div id="SettingsItem8" className="SettingsItem">
+                    <p>Placeholder Data 8</p>
+                    <p className="Switch">Switch</p>
+                </div>
+                <div id="SettingsItem9" className="SettingsItem">
+                    <p>Placeholder Data 9</p>
+                    <p className="Switch">Switch</p>
+                </div>
+                <div id="SettingsItem10" className="SettingsItem">
+                    <p>Placeholder Data 10</p>
+                    <p className="Switch">Switch</p>
+                </div>
             </div>
         );
     };
